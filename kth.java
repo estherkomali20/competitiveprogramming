8c2c0029c917860e03823d71524ff527913339a8
estherkomali20 <estherkomali09@gmail.com>
2026-07-27 07:32:15 +0530

    Add files via upload

---
+++ kth.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class kth {
+   
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        if ((n & (1 << k)) != 0) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+        sc.close();
+    }
+}
